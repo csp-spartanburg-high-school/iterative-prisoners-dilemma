66db9ca3249b14e35b056b3143a7683fc2500188
nathan <nathan12579111@gmail.com>
2020-01-09 10:42:03 -0500

    update
    update

--- fantastic umbrella.py
+++ fantastic umbrella.py
@@ -5,10 +5,9 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
 team_name = 'Team1' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'the winning strategy'
+strategy_description = 'we will do the winning'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
