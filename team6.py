4144e92101bfe8d75614ae35156854ed41d561c9
kuntz813 <59614487+kuntz813@users.noreply.github.com>
2020-01-10 10:44:36 -0500

    example
    discribe exaple

--- team6.py
+++ team6.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'Cube' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+strategy_name = 'nameo'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
